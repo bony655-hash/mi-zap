ee6d7a177e7b0bee2a37d901bad6cf35df80936b
bony655-hash <bony655@gmail.com>
2026-09-10 12:31:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
 // Service Worker de Mi Zapatería v3.5
 // IMPORTANTE: cambiar CACHE_VERSION con cada actualización de la app
 // para que los dispositivos descarguen la nueva versión automáticamente.
-const CACHE_VERSION = 55;
+const CACHE_VERSION = 60;
 const CACHE_NAME = 'zapateria-v' + CACHE_VERSION;
 
 const ARCHIVOS_CORE = [
   './index.html',
   './manifest.json',
   './icon-192.png',
+  './favicon.png',
   './icon-512.png'
 ];
 
